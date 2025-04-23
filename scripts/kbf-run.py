6d77beb303b283f518b701aebe9120fd0099580e
zakariaanzid <zakaria.anzid@eviden.com>
2025-04-23 17:08:49 +0200

    changing kbf-run

--- scripts/kbf-run.py
+++ scripts/kbf-run.py
@@ -7,7 +7,7 @@
 from kfp.client import Client
 
 #client = Client(host='localhost')
-client = Client()
+client = Client(host='http://ml-pipeline.kubeflow.svc.cluster.local:8888')
 
 run = client.create_run_from_pipeline_package(
     '/app/pipHello.yaml',
